bf6ed2382bff0ca4d2f22f1a830e74d8922e4357
rashedzahan <rashedzahan242gmail.com>
2017-11-21 01:28:50 +0600

    If Else Structure in C#

---
+++ ifElseStructures/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifElseStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //*********if statement
+
+            //     #region 
+
+            //     bool result = true;
+            //     if (result)
+            //      {
+            //          Console.WriteLine("The result is true !");
+            //          Console.ReadKey();
+            //      }
+            //      else
+            //    {
+            //          Console.WriteLine("The result is False !");
+            //         Console.ReadKey();
+            //    }
+
+            // #endregion
+
+//********************************************************************************
+
+            //********if...else if
+
+            //    #region
+            //   int value = 1;
+            //   if (value == 0)
+            //   {
+            //       Console.WriteLine("Value is 0 ");
+            //       Console.ReadKey();
+            //   }
+            //   else if(value == 1)
+            //   {
+            //       Console.WriteLine("Value is not 0 ");
+            //      Console.ReadKey();
+            //  }
+            //  else
+            //  {
+            //      Console.WriteLine("Value is something else ");
+            //     Console.ReadKey();
+            // }
+            // #endregion
+//********************************************************************************
+            // Switch Statement
+            //#region
+            //int value = 10;
+            //switch (value)
+            //{
+            //    case 0:
+            //        Console.WriteLine("Value is 0 ");
+            //        Console.ReadKey();
+            //        break;
+
+            //    case 1:
+            //        Console.WriteLine("Value is 1 ");
+            //        Console.ReadKey();
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Value is something else ");
+            //        Console.ReadKey();
+            //        break;
+            //}
+            //#endregion
+        }
+    }
+}
